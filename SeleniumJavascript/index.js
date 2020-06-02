d10d04c0d8bde0ea71156a37b3f58542c39871ef
sashikamw <sashimw@gmail.com>
2020-06-02 15:41:32 -0400

    update code

--- SeleniumJavascript/index.js
+++ SeleniumJavascript/index.js
@@ -1,7 +1,14 @@
-const{Builder, By, Key, util} = require("selenium-webdriver");
+//const{Builder, By, Key, util} = require("selenium-webdriver");
+var webdriver = require('selenium-webdriver');
+var chrome = require('selenium-webdriver/chrome');
+var path = require('chromedriver').path;
+
+var service = new chrome.ServiceBuilder(path).build();
+chrome.setDefaultService(service);
 
 async function login(){
-    let driver = await new Builder().forBrowser("chrome").build();
+    //let driver = await new Builder().forBrowser("chrome").build();
+    var driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
     await driver.get("https://consumer-dev.homexlabs.com/login");
     await driver.findElement(By.id("identifier")).sendKeys("development+test@homex.com");
     await driver.findElement(By.id("secret")).sendKeys("HOMEX@123");
